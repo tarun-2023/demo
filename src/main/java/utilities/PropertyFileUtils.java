cf28ede38c723d28710d8bcb5415921a389e8376
AST-LW <walkthroughfrom2020@gmail.com>
2024-01-26 22:29:01 +0530

    njd

--- src/main/java/utilities/PropertyFileUtils.java
+++ src/main/java/utilities/PropertyFileUtils.java
@@ -5,16 +5,27 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyFileUtils {
-    static String CONFIG_FILE_PATH = "src/main/resources/config.properties";
+    private static final String DEFAULT_CONFIG_FILE_PATH = "src/main/resources/";
+
     public static String getProperty(String propertyName) {
+        String env = System.getProperty("ENVIRONMENT");
+        String configFile = getConfigFile(env);
+
         Properties properties = new Properties();
-        try {
-            FileInputStream configFile = new FileInputStream(CONFIG_FILE_PATH);
-            properties.load(configFile);
+        try (FileInputStream fileInputStream = new FileInputStream(configFile)) {
+            properties.load(fileInputStream);
             return properties.getProperty(propertyName);
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            throw new IllegalStateException("Could not load properties from file: " + configFile, e);
+        }
+    }
+
+    private static String getConfigFile(String env) {
+        if (env == null || env.isEmpty()) {
+            return DEFAULT_CONFIG_FILE_PATH + "config.properties"; // Default config file
+        } else {
+            return DEFAULT_CONFIG_FILE_PATH + env + ".config.properties"; // Environment-specific config file
         }
     }
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -15,17 +15,18 @@ public class CartTest extends BaseAPITest {
 
     @Test
     public void addItemToCart() {
+        UserClient userClient = new UserClient();
+        ProductClient productClient = new ProductClient();
+        CartClient cartClient = new CartClient();
+
+        // Arrange
         String email = UUID.randomUUID() + "@ultralesson.com";
         String passwordFromDataLoader = DataLoader.getData("createCartData", "password");
         String productIDFromDataLoader = DataLoader.getData("createCartData", "product_id");
         int quantityFromDataLoader = Integer.parseInt (DataLoader.getData("createCartData", "quantity"));
 
-        UserClient userClient = new UserClient();
-        ProductClient productClient = new ProductClient();
-        CartClient cartClient = new CartClient();
-
+        // Act
         SignupResponseModel userSignupResponse = userClient.createUser(email, passwordFromDataLoader);
-
         String accessToken = userSignupResponse.getData().getSession().getAccessToken();
 
         ProductFetchResponseModel productFetchResponseModel = productClient.fetchProducts(accessToken);
@@ -36,12 +37,11 @@ public class CartTest extends BaseAPITest {
 
         AddItemToCartResponseModel addItemToCartResponseModel = cartClient.addItemCart(productIDFromDataLoader, accessToken, cartID, quantityFromDataLoader);
 
-        // Assertions
+        // Assert
         Assert.assertNotNull(addItemToCartResponseModel.getCartItemID());
         Assert.assertEquals( addItemToCartResponseModel.getProductID(), productId);
         Assert.assertEquals( addItemToCartResponseModel.getCartID(), cartID);
         Assert.assertEquals(addItemToCartResponseModel.getQuantity(), 10);
         Assert.assertEquals(addItemToCartResponseModel.getStatusCode(), 201);
     }
-
 }
